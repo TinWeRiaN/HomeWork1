d4430f4bedb4b7a50867e84e859222f68b94fa30
TinWeRiaN <13arnelika13@gmail.com>
2022-11-24 22:24:37 +0200

    finished homework 1 with tasks 1,2,3 added menu for choosing tasks

--- src/Calc/Calc.java
+++ src/Calc/Calc.java
@@ -1,16 +1,11 @@
-package Main;
+package Calc;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Calc {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
- //       System.out.println("Hello. What is your name?");
- //       String name = br.readLine();
-//        System.out.println("How old are you?");
-//        int age = Integer.parseInt(br.readLine());
 
         System.out.println("Inpute x");
         int x = Integer.parseInt(br.readLine());
@@ -18,8 +13,6 @@ public class Main {
         System.out.println("Inpute y");
         int y = Integer.parseInt(br.readLine());
 
- //       System.out.println("Hello " + name);
- //       System.out.println("You are " + age);
 
         System.out.println("x + y = " + (x+y));
         System.out.println("x - y = " + (x - y));
