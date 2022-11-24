ab6dc10bc1420b79d0b766e21720a12c5a542b58
TinWeRiaN <13arnelika13@gmail.com>
2022-11-24 22:37:37 +0200

    added forgotten main file

---
+++ src/All_HomeWorks/AllHomeWorks.java
@@ -0,0 +1,13 @@
+package All_HomeWorks;
+import All_HomeWorks.HomeWork1.HomeWork1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AllHomeWorks {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HomeWork1.doSelectTask();
+    }
+}
